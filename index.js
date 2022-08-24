4b91aeb9fc15c1980615fe692b29f6384fb49e4a
khadija.dahir <khadija.dahir@student.moringaschool.com>
2022-08-24 09:28:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record){
+    let winning = record.find(element => element.result == "W"
+    )
+    if (winning) {
+        return winning.year;
+    };
+    return;
+
+}
